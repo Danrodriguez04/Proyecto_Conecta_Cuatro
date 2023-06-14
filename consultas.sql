b77636538790b7b79d3821d14d451e8289035278
Alberto <agarciadel17@gmail.com>
2023-06-14 10:13:41 +0200

    Consultas hechas

--- consultas.sql
+++ consultas.sql
@@ -30,7 +30,77 @@ group by Usuario.id;
 
 /* 6 */
 
+select id_partida, count(id_partida) as total from comentario_partida
+group by id_partida
+having total > 2
+order by total desc;
+
+/* 7 */
+
+select id  from partida
+where not exists(select * from movimiento where id_partida = id);
+
+/*8*/
+
+select "Este año",usuario.id, traduccion_pais.nombre from usuario, ciudad,pais,traduccion_pais
+where usuario.id_ciudad = ciudad.id
+and ciudad.id_pais = pais.id
+and pais.id = traduccion_pais.id_pais
+and year(usuario.fecha_alta) = year(current_date())
+UNION
+select "Otro año", usuario.id, traduccion_pais.nombre from usuario, ciudad,pais,traduccion_pais
+where usuario.id_ciudad = ciudad.id
+and ciudad.id_pais = pais.id
+and pais.id = traduccion_pais.id_pais
+and year(usuario.fecha_alta) != year(current_date());
+
+/*9*/
+select truncate(avg(total_partida.total),1) from (
+select count(id_partida) as total from comentario_partida
+group by id_partida) as total_partida;
+
+/* 10 */
+
+select id from torneo where id_torneo_Dependiente = 1;
+
+/* 11 */
+
+select "Comentario Partidas" ,comentario_partida.id_usuario as Usuario,comentario_partida.id_partida as "Id partida/Movimiento",comentario_partida.comentario from comentario_partida ,usuario, ciudad,pais,traduccion_pais
+where comentario_partida.id_usuario = usuario.id
+and usuario.id_ciudad = ciudad.id
+and ciudad.id_pais = pais.id
+and traduccion_pais.id_pais = pais.id
+and traduccion_pais.nombre != "Spain"
+UNION
+select "Comentario Movimientos" ,comentario_movimiento.id_usuario,comentario_movimiento.id_movimiento,comentario_movimiento.comentario from comentario_movimiento,usuario, ciudad,pais,traduccion_pais
+where comentario_movimiento.id_usuario = usuario.id
+and usuario.id_ciudad = ciudad.id
+and ciudad.id_pais = pais.id
+and traduccion_pais.id_pais = pais.id
+and traduccion_pais.nombre != "Spain";
+
+/* 12 */
+
+select * from juego
+order by id_partida;
 
 
+select distinct id_usuario from juego as j1
+where id_usuario != (select  distinct j2.id_usuario from juego as j2,usuario, ciudad,pais,traduccion_pais
+					 where j1.id_partida = j2.id_partida
+					 and j1.id_usuario != j2.id_usuario
+					 and j2.id_usuario = usuario.id 
+					 and usuario.id_ciudad = ciudad.id
+					 and ciudad.id_pais = pais.id
+					 and traduccion_pais.id_pais = pais.id
+					 and traduccion_pais.nombre = "Spain")
+;
 
 
+/* Muestra usuarios de un pais en concreto.
+select  usuario.id from usuario, ciudad,pais,traduccion_pais
+where usuario.id_ciudad = ciudad.id
+and ciudad.id_pais = pais.id
+and traduccion_pais.id_pais = pais.id
+and traduccion_pais.nombre = "Colombia"
+*/
